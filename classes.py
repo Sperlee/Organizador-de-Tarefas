4ce95e8fe6af5e672a1008d298a6ae1afdf69cc5
Sperlee <sperlejoao@gmail.com>
2025-07-31 02:03:25 -0300

    Arrumado a parte de mostrar as tarefas com suas subtarefas

--- classes.py
+++ classes.py
@@ -3,17 +3,25 @@ from tkinter import ttk
 
 class Widget_de_tarefa:
     def __init__(self,widget,lista_tarefas):
-        self.linha = len(lista_tarefas) + 1 #linha onde está o botão da tarefa
-        self.botao_da_tarefa = ttk.Button(widget,text="nova_tarefa",command=lambda:mostrar_tasks(self))  # Mudou de widget_menu para widget
         self.username = StringVar()
         self.tarefa_concluida = BooleanVar(value=False)
         self.Nome_Tarefa = ttk.Entry(widget,width=60,textvariable=self.username)
         self.Marcador_Tarefa = ttk.Checkbutton(widget, variable=self.tarefa_concluida)
-        self.Botao_remover = ttk.Button(widget,text="remover",command=lambda: remover_tarefa(self,lista_tarefas))
+        self.Botao_remover = ttk.Button(widget,text="remover",command=lambda: remover_tarefa(self))
+
+class widget_das_subtarefas:
+    def __init__(self,widget,lista_tarefas):
+        self.NUM_TAREFAS = 5
+        self.linha = len(lista_tarefas) + 1 #linha onde está o botão da tarefa 
+        self.botao_da_tarefa = ttk.Button(widget,text="nova_tarefa",command=lambda:mostrar_tasks(self))# Mudou de widget_menu para widget
+        self.linhas = []
+        for i in range(self.NUM_TAREFAS):
+            task = Widget_de_tarefa(widget,lista_tarefas)
+            self.linhas.append(task)
 
 
 def criar_tarefa(lista_tarefas,widget):
-    nova_tarefa = Widget_de_tarefa(widget,lista_tarefas)
+    nova_tarefa = widget_das_subtarefas(widget,lista_tarefas)
     lista_tarefas.append(nova_tarefa)
 
     #botao para abrir as tarefas
@@ -21,17 +29,14 @@ def criar_tarefa(lista_tarefas,widget):
 
 
 def mostrar_tasks(tarefa):
-    tarefa.Nome_Tarefa.grid(row=tarefa.linha,column=1)
-    tarefa.Botao_remover.grid(row=tarefa.linha,column=3)
-    tarefa.Marcador_Tarefa.grid(row=tarefa.linha,column=2)
+    for i in range(tarefa.NUM_TAREFAS):
+        tarefa.linhas[i].Nome_Tarefa.grid(row=i+1,column=1)
+        tarefa.linhas[i].Marcador_Tarefa.grid(row=i+1,column=2)
+        tarefa.linhas[i].Botao_remover.grid(row=i+1,column=3)
 
-
-def remover_tarefa(tarefa_widget,lista_tarefas):
-    tarefa_widget.Nome_Tarefa.grid_forget()
-    tarefa_widget.Marcador_Tarefa.grid_forget()
-    tarefa_widget.Botao_remover.grid_forget()
+def remover_tarefa(tarefa_widget):
+    tarefa_widget.Nome_Tarefa.destroy()
+    tarefa_widget.Marcador_Tarefa.destroy()
+    tarefa_widget.Botao_remover.destroy()
     
-    if tarefa_widget in lista_tarefas:
-        tarefa_widget.botao_da_tarefa.grid_forget()
-        lista_tarefas.remove(tarefa_widget)
     
